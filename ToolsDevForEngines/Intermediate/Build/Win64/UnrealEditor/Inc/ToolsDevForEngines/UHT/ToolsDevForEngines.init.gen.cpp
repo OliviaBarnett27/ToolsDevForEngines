767ce01de73bc690418aa37dc7c84ea193d4aee5
OliviaBarnett27 <b013728n@gmail.com>
2024-12-09 15:37:54 +0000

    Add new files

---
+++ ToolsDevForEngines/Intermediate/Build/Win64/UnrealEditor/Inc/ToolsDevForEngines/UHT/ToolsDevForEngines.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeToolsDevForEngines_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_ToolsDevForEngines;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_ToolsDevForEngines()
+	{
+		if (!Z_Registration_Info_UPackage__Script_ToolsDevForEngines.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/ToolsDevForEngines",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x4BF65BA4,
+				0xFC2168C5,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_ToolsDevForEngines.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_ToolsDevForEngines.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ToolsDevForEngines(Z_Construct_UPackage__Script_ToolsDevForEngines, TEXT("/Script/ToolsDevForEngines"), Z_Registration_Info_UPackage__Script_ToolsDevForEngines, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x4BF65BA4, 0xFC2168C5));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
